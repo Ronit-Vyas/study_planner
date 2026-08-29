cc5ef561e63badea46361495dc6c678e6f860a98
Ronit-Vyas <ronitvyas85@gmail.com>
2026-08-29 16:06:45 +0530

    [fix]Updated models

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,22 +1,21 @@
 class StudyTask {
   final String id;
+
   final String courseId;
-  final String courseName;
-  final String title;
+  final String topicId;
+
   final DateTime date;
+
   final double duration;
-  final String priority;
 
   bool completed;
 
   StudyTask({
     required this.id,
     required this.courseId,
-    required this.courseName,
-    required this.title,
+    required this.topicId,
     required this.date,
     required this.duration,
-    required this.priority,
     this.completed = false,
   });
 }
---
+++ lib/models/topic_model.dart
@@ -0,0 +1,13 @@
+class Topic {
+  final String id;
+  final String courseId;
+  final String name;
+  final double estimatedHours;
+
+  Topic({
+    required this.id,
+    required this.courseId,
+    required this.name,
+    required this.estimatedHours,
+  });
+}
